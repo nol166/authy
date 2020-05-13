85da60a76351358ce823968acd27fb71aa8c189a
John McCambridge <jmccamb@gmail.com>
2020-05-13 17:23:43 -0500

    move auth information off the repo

--- config/keys.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    mongoURI: "mongodb+srv://jmccamb:Bobo!133@cluster0-tvwec.mongodb.net/test?retryWrites=true&w=majority"
-}
--- server.js
+++ server.js
@@ -1,9 +1,26 @@
-import express from 'express'
-import mongoose from 'mongoose'
-import bodyParser from 'body-parser'
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const mongoURI = process.env.MONGO_URI
+
+console.log(process.env.MONGO_URI)
 
 const app = express()
 
 // bodyparser middleware
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+
+mongoose
+    .connect(mongoURI, { useNewUrlParser: true })
+    .then((res) =>
+        console.log(
+            `MongoDB connected as ${res.connections[0].user} on ${res.connections[0].host}`
+        )
+    )
+    .catch((err) => console.log(err))
+
+const port = process.env.PORT || 5000 // the former is for heroku
+
+app.listen(port, () => console.log(`server running on port ${port}`))
