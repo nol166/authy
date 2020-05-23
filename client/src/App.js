f95e893018b809307b28b0c7c023e87ef0ab6c25
John McCambridge <jmccamb@gmail.com>
2020-05-23 10:21:53 -0500

    update landing and navbar components, add react router to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,17 @@
 import React from 'react'
+import Navbar from './components/layout/Navbar'
+import Landing from './components/layout/Landing'
 import './App.css'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 function App() {
     return (
-        <div className="App">
-            <h1> Hello World</h1>
-        </div>
+        <Router>
+            <div className="App">
+                <Navbar />
+                <Route exact path="/" component={Landing} />
+            </div>
+        </Router>
     )
 }
 
--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -10,15 +10,15 @@ class Landing extends Component {
                 <div className="row">
                     <div className="col s12 center-align">
                         <h4>
-                            <b>Build</b> a login/auth app with the{' '}
-                            <span style={{ fontFamily: 'monospace' }}>
+                            <b>Authy</b>{' '}
+                            {/* <span style={{ fontFamily: 'monospace' }}>
                                 MERN
                             </span>{' '}
-                            stack from scratch
+                            stack from scratch */}
                         </h4>
                         <p className="flow-text grey-text text-darken-1">
-                            Create a (minimal) full-stack app with user
-                            authentication via passport and JWTs
+                            "Neque porro quisquam est qui dolorem ipsum quia
+                            dolor sit amet, consectetur, adipisci velit..."
                         </p>
                         <br />
                         <div className="col s6">
@@ -42,7 +42,7 @@ class Landing extends Component {
                                     borderRadius: '3px',
                                     letterSpacing: '1.5px',
                                 }}
-                                className="btn btn-large btn-flat waves-effect white black-text"
+                                className="btn btn-large btn-flat waves-effect red black-text"
                             >
                                 Log In
                             </Link>
