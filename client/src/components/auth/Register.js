7dfb3b3922c1c25cb3bd05fd17657b3b0e16366a
John McCambridge <jmccamb@gmail.com>
2020-06-05 14:52:47 -0500

    add some new styles for register

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,138 +1,49 @@
-import React from 'react'
-import { useForm } from 'react-hook-form'
+import React, { useState } from 'react'
+import { isEmail, isEmpty } from 'validator'
+import { useForm, ErrorMessage } from 'react-hook-form'
 import './style/register.css'
 
-function Register() {
-    const { register, handleSubmit, errors } = useForm() // initialize the hook
-    const onSubmit = (data) => {
-        console.log(data)
+export default function Register() {
+    const [email, setEmail] = useState('')
+    const [username, setUserName] = useState('')
+    const [password, setPassword] = useState('')
+    const [password2, setPassword2] = useState('')
+    let errors = {}
+
+    const validateForm = ({ email, password, username, password2 }) => {
+        if (!isEmail(email)) {
+            errors.email = 'Not a valid email'
+        }
+        if (isEmpty(password) || isEmpty(password2)) {
+            errors.password = 'Please fill out both password fields'
+        }
+        if (isEmpty(username)) {
+            errors.username = 'Please provide a username'
+        }
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
     }
 
     return (
         <div className="form-container">
-            <form onSubmit={handleSubmit(onSubmit)} className="formS">
+            <form onSubmit={handleSubmit} className="">
+                <input type="email" name="email" />
+                <input type="username" name="username" placeholder="" />
                 <input
-                    name="email"
-                    ref={register({ required: true })}
-                    placeholder="john@jmdev.io"
-                />
-                {errors.email && 'Email required.'}
-                {/* register an input */}
-                <input
-                    name="name"
-                    ref={register({ required: true })}
-                    placeholder="username"
-                />
-                {errors.name && 'User name is required.'}
-                <input
+                    type="password"
                     name="password"
-                    ref={register({ required: true })}
-                    placeholder="*******"
+                    placeholder="person@jmdev.io"
                 />
-                {errors.password && 'Please enter password.'}
                 <input
+                    type="passwword"
                     name="password2"
-                    ref={register({ required: true })}
-                    placeholder="*******"
+                    placeholder="person@jmdev.io"
                 />
-                {errors.password2 && 'Please confirm number for password.'}
-                <button
-                    className="btn waves-effect waves-light"
-                    type="submit"
-                    name="action"
-                >
-                    register
-                    <i className="material-icons right" />
-                </button>
+                <input type="email" placeholder="person@jmdev.io" />
+                <input type="submit" />
             </form>
         </div>
-
-        //     <div id="register-page" className="row" style={{ width: '30%' }}>
-        //         <div className="col s12 z-depth-6 card-panel">
-        //             <form
-        //                 className="register-form"
-        //                 style={{ width: '90%', paddingTop: '10px' }}
-        //                 onSubmit={handleSubmit(onSubmit)}
-        //             >
-        //                 <div className="row margin">
-        //                     <div className="input-field col s12">
-        //                         <i className="mdi-social-person-outline prefix"></i>
-        //                         <input
-        //                             name="user_name"
-        //                             type="text"
-        //                             className="validate"
-        //                         />
-        //                         <label htmlFor="user_name" className="center-align">
-        //                             Username
-        //                         </label>
-        //                     </div>
-        //                 </div>
-        //                 <div className="row margin">
-        //                     <div className="input-field col s12">
-        //                         <i className="mdi-communication-email prefix"></i>
-        //                         <input
-        //                             id="user_email"
-        //                             type="email"
-        //                             className="validate"
-        //                             ref={register}
-        //                         />
-        //                         <label
-        //                             htmlFor="user_email"
-        //                             className="center-align"
-        //                         >
-        //                             Email
-        //                         </label>
-        //                     </div>
-        //                 </div>
-        //                 <div className="row margin">
-        //                     <div className="input-field col s12">
-        //                         <i className="mdi-action-lock-outline prefix"></i>
-        //                         <input
-        //                             id="user_passw"
-        //                             type="password"
-        //                             className="validate"
-        //                             ref={register}
-        //                         />
-        //                         <label htmlFor="user_passw">Password</label>
-        //                     </div>
-        //                 </div>
-        //                 <div className="row margin">
-        //                     <div className="input-field col s12">
-        //                         <i className="mdi-action-lock-outline prefix"></i>
-        //                         <input
-        //                             id="confirm_pass"
-        //                             type="password"
-        //                             ref={register}
-        //                         />
-        //                         <label htmlFor="confirm_pass">
-        //                             Re-type password
-        //                         </label>
-        //                     </div>
-        //                 </div>
-        //                 <div className="row">
-        //                     <div
-        //                         className="input-field col s12"
-        //                         style={{ margin: '10px' }}
-        //                     >
-        //                         <a
-        //                             href="register.html"
-        //                             className="btn waves-effect waves-light col s12"
-        //                         >
-        //                             Register Now
-        //                         </a>
-        //                     </div>
-        //                     <div className="input-field col s12">
-        //                         <p className="margin center medium-small sign-up">
-        //                             Already have an account?{' '}
-        //                             <Link to="/login">Login</Link>
-        //                         </p>
-        //                     </div>
-        //                 </div>
-        //             </form>
-        //         </div>
-        //     </div>
-        // )
     )
 }
-
-export default Register
